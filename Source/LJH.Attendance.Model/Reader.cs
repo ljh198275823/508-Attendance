939d616b5c030ba10c9ac40c4430e3cdabe8c832
ljh198275823 <ljh198275823@163.com>
2013-10-25 19:02:34 +0800

    1

--- Source/LJH.Attendance.Model/Reader.cs
+++ Source/LJH.Attendance.Model/Reader.cs
@@ -26,6 +26,42 @@ namespace LJH.Attendance.Model
         /// </summary>
         public string Name { get; set; }
         /// <summary>
+        /// 获取或设置设备类型
+        /// </summary>
+        public byte DeviceType { get; set; }
+        /// <summary>
+        /// 获取或设置IP
+        /// </summary>
+        public string IP { get; set; }
+        /// <summary>
+        /// 获取或设置子网掩码
+        /// </summary>
+        public string IPMask { get; set; }
+        /// <summary>
+        /// 获取或设置网关
+        /// </summary>
+        public string Gateway { get; set; }
+        /// <summary>
+        /// 获取或设置控制端口号
+        /// </summary>
+        public int ControlPort { get; set; }
+        /// <summary>
+        /// 获取或设置事件端口号
+        /// </summary>
+        public int EventPort { get; set; }
+        /// <summary>
+        /// 获取或设置MAC地址
+        /// </summary>
+        public string MACAddress { get; set; }
+        /// <summary>
+        /// 获取或设置串口号
+        /// </summary>
+        public byte Commport { get; set; }
+        /// <summary>
+        /// 获取或设置地址
+        /// </summary>
+        public int Address { get; set; }
+        /// <summary>
         /// 获取或设置是否用于考勤
         /// </summary>
         public bool ForAttendance { get; set; }
