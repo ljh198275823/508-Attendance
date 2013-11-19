2faac68b2dafad5df90207e1353004049a471fda
ljh198275823 <ljh198275823@163.com>
2013-11-19 23:07:03 +0800

    1.开始增加中控指纹机设备

---
+++ Source/Device/ClearDataFlag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LJH.Attendance.Device
+{
+    [Flags]
+    public enum ClearDataFlag
+    {
+        /// <summary>
+        /// 全部
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 考勤记录/刷卡记录
+        /// </summary>
+        AttendanceLog = 0x01,
+        /// <summary>
+        /// 指纹模板
+        /// </summary>
+        FPTemplate = 0x02,
+        /// <summary>
+        /// 操作记录
+        /// </summary>
+        OperatorLog = 0x04,
+        /// <summary>
+        /// 用户信息
+        /// </summary>
+        UserInfo = 0x08,
+        /// <summary>
+        /// 全部
+        /// </summary>
+        All = 0xFFFF,
+    }
+}
--- Source/Device/ZKTFTFingerReader.cs
+++ Source/Device/ZKTFTFingerReader.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using zkemkeeper;
+using LJH.Attendance.Model;
 
 namespace LJH.Attendance.Device
 {
@@ -21,37 +23,130 @@ namespace LJH.Attendance.Device
         }
         #endregion
 
+        #region 私有变量
+        private zkemkeeper.CZKEM axCZKEM1 = new zkemkeeper.CZKEM();
+
+        private bool _Connected = false;
+
+        private int _MachineNumber = 1;
+        #endregion
+
         #region 公共属性
+        /// <summary>
+        /// 获取或设置设备的参数
+        /// </summary>
         public LJH.Attendance.Model.DeviceInfo Parameter { get; set; }
         #endregion
 
         #region 实现接口
+        /// <summary>
+        /// 连接设备
+        /// </summary>
         public void Connect()
         {
-            if (Parameter != null)
+            if (Parameter != null && !_Connected)
             {
-                
+                if (Parameter.Communication == CommunicationType.TCP_IP && !string.IsNullOrEmpty(Parameter.IP) && Parameter.ControlPort != null)
+                {
+                    _Connected = axCZKEM1.Connect_Net(Parameter.IP, Parameter.ControlPort.Value);
+                }
+                else if (Parameter.Communication == CommunicationType.RS232_485 && Parameter.Commport != null && Parameter.Baud != null)
+                {
+                    _Connected = axCZKEM1.Connect_Com(Parameter.Commport.Value, _MachineNumber, Parameter.Baud.Value);
+                }
+                if (!_Connected)
+                {
+                    int idwErrorCode = 0;
+                    axCZKEM1.GetLastError(ref idwErrorCode);
+                    LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "连接设备失败,ErrorCode=" + idwErrorCode.ToString());
+                }
+            }
+        }
+        /// <summary>
+        /// 断开设备
+        /// </summary>
+        public void Disconnect()
+        {
+            if (_Connected)
+            {
+                axCZKEM1.Disconnect();
             }
         }
 
-        public void Disconnect()
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// 获取是否连接成功
+        /// </summary>
         public bool IsConnected
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                return _Connected;
+            }
         }
 
-        public List<Model.AttendanceLog> GetAttendanceLogs()
+
+        public List<AttendanceLog> GetAttendanceLogs()
         {
             throw new NotImplementedException();
         }
 
-        public void SetUserInfo(Model.Staff staff)
+        public void SetUserInfo(Staff staff)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 清空数据
+        /// </summary>
+        /// <param name="flag"></param>
+        public void ClearData(ClearDataFlag flag)
+        {
+            if (!_Connected)
+            {
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "清空失败，未连接设备");
+                return;
+            }
+            bool ret = false;
+            if ((flag & ClearDataFlag.AttendanceLog) == ClearDataFlag.AttendanceLog)
+            {
+                ret = axCZKEM1.ClearData(_MachineNumber, 1);
+                if (!ret)
+                {
+                    int idwErrorCode = 0;
+                    axCZKEM1.GetLastError(ref idwErrorCode);
+                    LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "清空考勤记录失败,ErrorCode=" + idwErrorCode.ToString());
+                }
+            }
+            if ((flag & ClearDataFlag.FPTemplate) == ClearDataFlag.FPTemplate)
+            {
+                ret = axCZKEM1.ClearData(_MachineNumber, 2);
+                if (!ret)
+                {
+                    int idwErrorCode = 0;
+                    axCZKEM1.GetLastError(ref idwErrorCode);
+                    LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "清空指纹模板失败,ErrorCode=" + idwErrorCode.ToString());
+                }
+            }
+            if ((flag & ClearDataFlag.OperatorLog) == ClearDataFlag.OperatorLog)
+            {
+                ret = axCZKEM1.ClearData(_MachineNumber, 4);
+                if (!ret)
+                {
+                    int idwErrorCode = 0;
+                    axCZKEM1.GetLastError(ref idwErrorCode);
+                    LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "清空操作记录失败,ErrorCode=" + idwErrorCode.ToString());
+                }
+            }
+            if ((flag & ClearDataFlag.UserInfo) == ClearDataFlag.UserInfo)
+            {
+                ret = axCZKEM1.ClearData(_MachineNumber, 5);
+                if (!ret)
+                {
+                    int idwErrorCode = 0;
+                    axCZKEM1.GetLastError(ref idwErrorCode);
+                    LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "清空用户信息失败,ErrorCode=" + idwErrorCode.ToString());
+                }
+            }
         }
         #endregion
     }
