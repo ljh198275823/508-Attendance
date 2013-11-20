5e0b265f296ccc1f0582c23c2eaa8f811a66d4c8
ljh198275823 <ljh198275823@163.com>
2013-11-20 23:16:36 +0800

    1.

--- Source/Device/ZKTFTFingerKeeper.cs
+++ Source/Device/ZKTFTFingerKeeper.cs
@@ -10,14 +10,14 @@ namespace LJH.Attendance.Device
     /// <summary>
     /// 表示中控彩屏指纹读卡器
     /// </summary>
-    public class ZKTFTFingerReader : IReader
+    public class ZKTFTFingerKeeper : IReader
     {
         #region 构造函数
-        public ZKTFTFingerReader()
+        public ZKTFTFingerKeeper()
         {
         }
 
-        public ZKTFTFingerReader(LJH.Attendance.Model.DeviceInfo para)
+        public ZKTFTFingerKeeper(LJH.Attendance.Model.DeviceInfo para)
         {
             Parameter = para;
         }
@@ -28,7 +28,7 @@ namespace LJH.Attendance.Device
 
         private bool _Connected = false;
 
-        private int _MachineNumber = 1;
+        private int iMachineNumber = 1;
         #endregion
 
         #region 公共属性
@@ -52,7 +52,7 @@ namespace LJH.Attendance.Device
                 }
                 else if (Parameter.Communication == CommunicationType.RS232_485 && Parameter.Commport != null && Parameter.Baud != null)
                 {
-                    _Connected = axCZKEM1.Connect_Com(Parameter.Commport.Value, _MachineNumber, Parameter.Baud.Value);
+                    _Connected = axCZKEM1.Connect_Com(Parameter.Commport.Value, iMachineNumber, Parameter.Baud.Value);
                 }
                 if (!_Connected)
                 {
@@ -89,12 +89,25 @@ namespace LJH.Attendance.Device
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// 保存用户
+        /// </summary>
+        /// <param name="staff"></param>
         public void SetUserInfo(Staff staff)
         {
-            throw new NotImplementedException();
+            if (!_Connected)
+            {
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "保存用户信息失败，未连接设备");
+                return;
+            }
+            bool ret = axCZKEM1.SSR_SetUserInfo(iMachineNumber, staff.ID.ToString(), staff.Name, "8888", 0, true);
+            if (!ret)
+            {
+                int idwErrorCode = 0;
+                axCZKEM1.GetLastError(ref idwErrorCode);
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "保存用户信息失败，ErrorCode=" + idwErrorCode.ToString());
+            }
         }
-
         /// <summary>
         /// 清空数据
         /// </summary>
@@ -109,7 +122,7 @@ namespace LJH.Attendance.Device
             bool ret = false;
             if ((flag & ClearDataFlag.AttendanceLog) == ClearDataFlag.AttendanceLog)
             {
-                ret = axCZKEM1.ClearData(_MachineNumber, 1);
+                ret = axCZKEM1.ClearData(iMachineNumber, 1);
                 if (!ret)
                 {
                     int idwErrorCode = 0;
@@ -119,7 +132,7 @@ namespace LJH.Attendance.Device
             }
             if ((flag & ClearDataFlag.FPTemplate) == ClearDataFlag.FPTemplate)
             {
-                ret = axCZKEM1.ClearData(_MachineNumber, 2);
+                ret = axCZKEM1.ClearData(iMachineNumber, 2);
                 if (!ret)
                 {
                     int idwErrorCode = 0;
@@ -129,7 +142,7 @@ namespace LJH.Attendance.Device
             }
             if ((flag & ClearDataFlag.OperatorLog) == ClearDataFlag.OperatorLog)
             {
-                ret = axCZKEM1.ClearData(_MachineNumber, 4);
+                ret = axCZKEM1.ClearData(iMachineNumber, 4);
                 if (!ret)
                 {
                     int idwErrorCode = 0;
@@ -139,13 +152,87 @@ namespace LJH.Attendance.Device
             }
             if ((flag & ClearDataFlag.UserInfo) == ClearDataFlag.UserInfo)
             {
-                ret = axCZKEM1.ClearData(_MachineNumber, 5);
+                ret = axCZKEM1.ClearData(iMachineNumber, 5);
                 if (!ret)
                 {
                     int idwErrorCode = 0;
                     axCZKEM1.GetLastError(ref idwErrorCode);
                     LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "清空用户信息失败,ErrorCode=" + idwErrorCode.ToString());
                 }
+            }
+        }
+        /// <summary>
+        /// 设置时间
+        /// </summary>
+        /// <param name="dt"></param>
+        public void SetTime(DateTime dt)
+        {
+            if (!_Connected)
+            {
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "设置时间失败，未连接设备");
+                return;
+            }
+            int idwErrorCode = 0;
+            if (axCZKEM1.SetDeviceTime2(iMachineNumber, dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second))
+            {
+                axCZKEM1.RefreshData(iMachineNumber);//the data in the device should be refreshed
+            }
+            else
+            {
+                axCZKEM1.GetLastError(ref idwErrorCode);
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "设置时间失败，,ErrorCode=" + idwErrorCode.ToString());
+            }
+        }
+        /// <summary>
+        /// 重启
+        /// </summary>
+        public void Restart()
+        {
+            if (!_Connected)
+            {
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "重启失败，未连接设备");
+                return;
+            }
+            if (!axCZKEM1.RestartDevice(iMachineNumber) == true)
+            {
+                int idwErrorCode = 0;
+                axCZKEM1.GetLastError(ref idwErrorCode);
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "重启失败，ErrorCode=" + idwErrorCode.ToString());
+            }
+        }
+        /// <summary>
+        /// 关机
+        /// </summary>
+        public void PowerOff()
+        {
+            if (!_Connected)
+            {
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "关机失败，未连接设备");
+                return;
+            }
+            if (!axCZKEM1.PowerOffDevice(iMachineNumber) == true)
+            {
+                int idwErrorCode = 0;
+                axCZKEM1.GetLastError(ref idwErrorCode);
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "关机失败，ErrorCode=" + idwErrorCode.ToString());
+            }
+        }
+        /// <summary>
+        /// 重新设置IP地址
+        /// </summary>
+        /// <param name="newIP"></param>
+        public void SetIP(string newIP)
+        {
+            if (!_Connected)
+            {
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "设置IP失败，未连接设备");
+                return;
+            }
+            if (!axCZKEM1.SetDeviceIP(iMachineNumber, newIP) == true)
+            {
+                int idwErrorCode = 0;
+                axCZKEM1.GetLastError(ref idwErrorCode);
+                LJH.GeneralLibrary.LOG.FileLog.Log(Parameter.Name, "设置IP失败，ErrorCode=" + idwErrorCode.ToString());
             }
         }
         #endregion
