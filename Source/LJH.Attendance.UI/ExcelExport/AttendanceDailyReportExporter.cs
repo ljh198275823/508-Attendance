eeb2be8b9973e04b05bbccbf2c45debfbc4a4c98
bruce <ljh198275823@163.com>
2014-03-25 17:27:53 +0800

    1.增加模板导入功能

---
+++ Source/LJH.Attendance.UI/ExcelExport/AttendanceDailyReportExporter.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+using LJH.Attendance.Model;
+using LJH.Attendance.Model.SearchCondition;
+using LJH.Attendance.BLL;
+using LJH.GeneralLibrary;
+
+namespace LJH.Attendance.UI.ExcelExport
+{
+    public class AttendanceDailyReportExporter
+    {
+        #region 构造函数
+        public AttendanceDailyReportExporter(string modalPath)
+        {
+            ReportModal = modalPath;
+        }
+        #endregion
+
+        #region 私有变量
+        private int _CurrentRow = 0;
+        #endregion
+
+        #region 私有方法
+        private void FillRow(Staff staff, List<AttendanceResult> ars, Department dept, Worksheet sheet)
+        {
+            int row = _CurrentRow;
+            Range r = sheet.get_Range(GetColName(1) + row, Type.Missing); //卡号
+            r.Value = staff.CardID;
+            r = sheet.get_Range(GetColName(2) + row, Type.Missing); //人员编号
+            r.Value = staff.Certificate;
+            r = sheet.get_Range(GetColName(3) + row, Type.Missing);//姓名
+            r.Value = staff.Name;
+            r = sheet.get_Range(GetColName(4) + row, Type.Missing);//电邮
+            //r.Value = staff.Email;
+            r = sheet.get_Range(GetColName(5) + row, Type.Missing);//所在单位
+            r.Value = dept != null ? dept.Name : string.Empty;
+            foreach (AttendanceResult ar in ars)
+            {
+                r = sheet.get_Range(GetColName(6) + row, Type.Missing);//日期
+                r.Value = ar.ShiftDate;
+                r = sheet.get_Range(GetColName(7) + row, Type.Missing);//应上班
+                r.Value = ar.StartTime.ToString("HH:mm");
+                r = sheet.get_Range(GetColName(8) + row, Type.Missing);//实上班
+                r.Value = ar.OnDutyTime != null ? ar.OnDutyTime.Value.ToString("HH:mm") : "--";
+                if (ar.OnDutyTime == null || ar.Result == AttendanceResultCode.Late || ar.Result == AttendanceResultCode.LateEarly)
+                {
+                    r.Font.Color = System.Drawing.Color.Red;
+                    r.Font.Bold = 1;
+                    sheet.get_Range(GetColName(6) + row, GetColName(13) + row).Interior.Color = System.Drawing.Color.Yellow;
+                }
+                r = sheet.get_Range(GetColName(9) + row, Type.Missing);//应下班
+                r.Value = ar.EndTime.ToString("HH:mm");
+                r = sheet.get_Range(GetColName(10) + row, Type.Missing);//实下班
+                r.Value = ar.OffDutyTime != null ? ar.OffDutyTime.Value.ToString("HH:mm") : "--";
+                if (ar.OffDutyTime == null || ar.Result == AttendanceResultCode.LeaveEarly || ar.Result == AttendanceResultCode.LateEarly)
+                {
+                    r.Font.Color = System.Drawing.Color.Red;
+                    r.Font.Bold = 1;
+                    sheet.get_Range(GetColName(6) + row, GetColName(13) + row).Interior.Color = System.Drawing.Color.Yellow;
+                }
+                r = sheet.get_Range(GetColName(11) + row, Type.Missing);//考勤结果
+                r.Value = ar.ResultDescr;
+                r = sheet.get_Range(GetColName(12) + row, Type.Missing);//应出勤(h)
+                r.Value = (ar.ShiftTime / 60).Trim();
+                r = sheet.get_Range(GetColName(13) + row, Type.Missing);//实出勤(h)
+                r.Value = (ar.Present / 60).Trim();
+                row++;
+            }
+            sheet.get_Range(GetColName(1) + _CurrentRow, GetColName(1) + (row - 1)).Merge(Type.Missing);
+            sheet.get_Range(GetColName(2) + _CurrentRow, GetColName(2) + (row - 1)).Merge(Type.Missing);
+            sheet.get_Range(GetColName(3) + _CurrentRow, GetColName(3) + (row - 1)).Merge(Type.Missing);
+            sheet.get_Range(GetColName(4) + _CurrentRow, GetColName(4) + (row - 1)).Merge(Type.Missing);
+            sheet.get_Range(GetColName(5) + _CurrentRow, GetColName(5) + (row - 1)).Merge(Type.Missing);
+            _CurrentRow = row;
+        }
+
+        private string GetColName(int col)
+        {
+            string temp = string.Empty;
+            int t1 = col / 27;
+            if (t1 == 1) temp = "A";
+            if (t1 == 2) temp = "B";
+            if (t1 == 3) temp = "C";
+            int t2 = col - t1 * 26;
+            char ch = (char)((int)'A' + t2 - 1);
+            temp = temp + ch.ToString();
+            return temp;
+        }
+
+        private void AddBorder(Range r)
+        {
+            r.Borders.LineStyle = XlLineStyle.xlContinuous;
+        }
+        #endregion
+
+        #region 公共属性
+        /// <summary>
+        /// 获取或设置EXCEL模板的路径
+        /// </summary>
+        public string ReportModal { get; set; }
+        #endregion
+
+        #region 公共方法
+        /// <summary>
+        /// 导出相关人员在某一段时间的考勤报表
+        /// </summary>
+        /// <param name="staff"></param>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        public void Export(List<Staff> users, int year, int month, string path)
+        {
+            Workbook book = null;
+            try
+            {
+                Application app = new Application();
+                book = app.Workbooks.Add(ReportModal); //打开模板
+                Worksheet sheet = book.ActiveSheet as Worksheet;
+
+                if (users != null && users.Count > 0)
+                {
+                    List<Department> depts = (new DepartmentBLL(AppSettings.CurrentSetting.ConnectString)).GetItems(null).QueryObjects;
+                    List<int> staff = users.Select(item => item.ID).ToList();
+                    StaffAttendanceResultSearchCondition con = new StaffAttendanceResultSearchCondition();
+                    con.Staff = staff;
+                    DateTime dt = new DateTime(year, month, 1);
+                    con.ShiftDate = new DatetimeRange(dt, dt.AddMonths(1).AddSeconds(-1));
+                    List<AttendanceResult> arranges = (new AttendanceResultBLL(AppSettings.CurrentSetting.ConnectString)).GetItems(con).QueryObjects;
+                    _CurrentRow = 2;
+                    foreach (Staff s in users)
+                    {
+                        List<AttendanceResult> rets = (from it in arranges where it.StaffID == s.ID orderby it.StartTime ascending select it).ToList();
+                        if (rets != null && rets.Count > 0)
+                        {
+                            Department dept = depts.SingleOrDefault(it => it.ID == s.DepartmentID);
+                            FillRow(s, rets, dept, sheet);
+                        }
+                    }
+                    sheet.get_Range("A1", GetColName(13) + (_CurrentRow - 1)).Borders.LineStyle = XlLineStyle.xlContinuous;
+                }
+                book.SaveAs(path, XlFileFormat.xlXMLSpreadsheet, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            }
+            catch (Exception ex)
+            {
+                LJH.GeneralLibrary.ExceptionHandling.ExceptionPolicy.HandleException(ex);
+            }
+            finally
+            {
+                book.Close(false, Type.Missing, Type.Missing);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Source/LJH.Attendance.UI/ExcelExport/AttendanceMonthReportExporter.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+using LJH.Attendance .Model ;
+using LJH.Attendance.Model.SearchCondition;
+using LJH.Attendance .BLL ;
+using LJH.GeneralLibrary;
+
+namespace LJH.Attendance.UI.ExcelExport
+{
+    /// <summary>
+    /// 表示考勤月统计报表导出类
+    /// </summary>
+    public class AttendanceMonthReportExporter
+    {
+        public AttendanceMonthReportExporter(string modalPath)
+        {
+            ReportModal = modalPath;
+        }
+
+        #region 公共属性
+        /// <summary>
+        /// 获取或设置EXCEL模板的路径
+        /// </summary>
+        public string ReportModal { get; set; }
+        #endregion
+
+        #region 公共方法
+        /// <summary>
+        /// 导出相关人员在某一段时间的考勤报表
+        /// </summary>
+        /// <param name="staff"></param>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        public void Export(List<Staff> users, int year, int month, string path)
+        {
+            Workbook book = null;
+            try
+            {
+                Application app = new Application();
+                book = app.Workbooks.Add(ReportModal); //打开模板
+                Worksheet sheet = book.ActiveSheet as Worksheet;
+                Exportweek(year, month, sheet);
+
+                if (users != null && users.Count > 0)
+                {
+                    List<Department> depts = (new DepartmentBLL(AppSettings.CurrentSetting.ConnectString)).GetItems(null).QueryObjects;
+                    List<int> staff = users.Select(item => item.ID).ToList();
+                    StaffAttendanceResultSearchCondition con = new StaffAttendanceResultSearchCondition();
+                    con.Staff = staff;
+                    DateTime dt = new DateTime(year, month, 1);
+                    con.ShiftDate = new DatetimeRange(dt, dt.AddMonths(1).AddSeconds(-1));
+                    List<AttendanceResult> arranges = (new AttendanceResultBLL(AppSettings.CurrentSetting.ConnectString)).GetItems(con).QueryObjects;
+                    int row = 1;
+                    foreach (Staff s in users)
+                    {
+                        List<AttendanceResult> rets = (from it in arranges where it.StaffID == s.ID orderby it.StartTime ascending select it).ToList();
+                        if (rets != null && rets.Count > 0)
+                        {
+                            AddStaff(s, rets, depts, sheet, row);
+                            row++;
+                        }
+                    }
+                    sheet.get_Range("A1", GetColName(49) + (2 + (row - 1) * 3)).Borders.LineStyle = XlLineStyle.xlContinuous;
+                }
+                book.SaveAs(path, XlFileFormat.xlXMLSpreadsheet, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            }
+            catch (Exception ex)
+            {
+                LJH.GeneralLibrary.ExceptionHandling.ExceptionPolicy.HandleException(ex);
+            }
+            finally
+            {
+                book.Close(false, Type.Missing, Type.Missing);
+            }
+        }
+
+        private void Exportweek(int year, int month, Worksheet sheet)
+        {
+            DateTime dtBegin = new DateTime(year, month, 1);
+            for (int i = 0; i < 31; i++)
+            {
+                DateTime dt = dtBegin.AddDays(i);
+                string week = string.Empty;
+                if (dt.DayOfWeek == DayOfWeek.Sunday) week = "日";
+                else if (dt.DayOfWeek == DayOfWeek.Monday) week = "一";
+                else if (dt.DayOfWeek == DayOfWeek.Tuesday) week = "二";
+                else if (dt.DayOfWeek == DayOfWeek.Wednesday) week = "三";
+                else if (dt.DayOfWeek == DayOfWeek.Thursday) week = "四";
+                else if (dt.DayOfWeek == DayOfWeek.Friday) week = "五";
+                else if (dt.DayOfWeek == DayOfWeek.Saturday) week = "六";
+                Range r = sheet.get_Range(GetColName(10 + i) + 2, Type.Missing);
+                r.Value = week;
+                if (week == "日")
+                {
+                    //sheet.get_Range (GetColName(10 + i)+65535)
+                }
+            }
+        }
+
+        private void AddStaff(Staff staff, List<AttendanceResult> items, List<Department> depts, Worksheet sheet, int row)
+        {
+            int myRow = 3 + (row - 1) * 3; //从第三行开始输入数据,每个人占三行
+            Range r = sheet.get_Range(GetColName(1) + myRow, Type.Missing); //序号
+            r.Value = row;
+            Department dept = depts.SingleOrDefault(it => it.ID == staff.DepartmentID);
+            Department root = null;
+            if (dept != null) root = GetRoot(depts, dept);
+            r = sheet.get_Range(GetColName(2) + myRow, Type.Missing); //中心
+            r.Value = root != null ? root.Name : string.Empty;
+            r = sheet.get_Range(GetColName(3) + myRow, Type.Missing); //部门
+            r.Value = dept != null ? dept.Name : string.Empty;
+            r = sheet.get_Range(GetColName(5) + myRow, Type.Missing);  //职位
+            r.Value = staff.UserPosition;
+            r = sheet.get_Range(GetColName(6) + myRow, Type.Missing);  //姓名
+            r.Value = staff.Name;
+            r = sheet.get_Range(GetColName(9) + myRow, Type.Missing);  //
+            r.Value = "上午";
+            r = sheet.get_Range(GetColName(9) + (myRow + 1), Type.Missing);  //
+            r.Value = "下午";
+            r = sheet.get_Range(GetColName(9) + (myRow + 2), Type.Missing);  //
+            r.Value = "加班";
+            for (int i = 1; i <= 8; i++)
+            {
+                sheet.get_Range(GetColName(i) + myRow, GetColName(i) + (myRow + 2)).Merge(Type.Missing);
+            }
+            for (int i = 1; i <= 31; i++)
+            {
+                List<AttendanceResult> rets = (from item in items where item.ShiftDate.Day == i && !string.IsNullOrEmpty(item.ShiftID) orderby item.StartTime ascending select item).ToList();
+                if (rets != null && rets.Count > 0)
+                {
+                    if (rets.Count >= 1)
+                    {
+                        r = sheet.get_Range(GetColName(9 + i) + (myRow + 0), Type.Missing);
+                        r.Value = (rets[0].Present / 60).Trim();
+                    }
+                    if (rets.Count >= 2)
+                    {
+                        r = sheet.get_Range(GetColName(9 + i) + (myRow + 1), Type.Missing);
+                        r.Value = (rets[1].Present / 60).Trim(); ;
+                    }
+                }
+                decimal ot = (from item in items where item.ShiftDate.Day == i && string.IsNullOrEmpty(item.ShiftID) && !string.IsNullOrEmpty(item.Category) select item).Sum(item => item.Present);
+                if (ot > 0)
+                {
+                    r = sheet.get_Range(GetColName(9 + i) + (myRow + 2), Type.Missing);
+                    r.Value = (ot / 60).Trim();
+                }
+            }
+            for (int i = 41; i <= 49; i++)
+            {
+                sheet.get_Range(GetColName(i) + myRow, GetColName(i) + (myRow + 2)).Merge(Type.Missing);
+            }
+            sheet.get_Range(GetColName(41) + myRow).Value = string.Format("=SUM(J{0}:AN{1})", myRow + 2, myRow + 2);
+            sheet.get_Range(GetColName(42) + myRow).Value = string.Format("=COUNTIF(J{0}:AN{1},\"△\")*4", myRow, myRow + 1);
+            sheet.get_Range(GetColName(45) + myRow).Value = string.Format("=COUNTIF(J{0}:AN{1},\"休\")*4", myRow, myRow + 1);
+            sheet.get_Range(GetColName(46) + myRow).Value = string.Format("=SUM(J{0}:AN{1})", myRow, myRow + 1);
+            sheet.get_Range(GetColName(48) + myRow).Value = string.Format("=AS{0}+AT{1}", myRow, myRow); //总小时数
+        }
+
+        private Department GetRoot(List<Department> depts, Department dept)
+        {
+            Department d = dept;
+            while (!string.IsNullOrEmpty(d.ParentID))
+            {
+                Department temp = depts.SingleOrDefault(it => it.ID == d.ParentID);
+                if (temp == null) break;
+                d = temp;
+            }
+            return d;
+        }
+
+        private string GetColName(int col)
+        {
+            string temp = string.Empty;
+            int t1 = col / 27;
+            if (t1 == 1) temp = "A";
+            if (t1 == 2) temp = "B";
+            if (t1 == 3) temp = "C";
+            int t2 = col - t1 * 26;
+            char ch = (char)((int)'A' + t2 - 1);
+            temp = temp + ch.ToString();
+            return temp;
+        }
+
+        private void AddBorder(Range r)
+        {
+            r.Borders.LineStyle = XlLineStyle.xlContinuous;
+        }
+        #endregion
+    }
+}
